d03bb09ca35f392a7087396f8ae77328677b65d4
quim-motger <quim.motger@gmail.com>
2015-09-25 12:08:34 +0200

    models selected in ModelSelectionDialog

--- riscoss-webapp/src/main/java/eu/riscoss/client/riskconfs/ModelSelectionDialog.java
+++ riscoss-webapp/src/main/java/eu/riscoss/client/riskconfs/ModelSelectionDialog.java
@@ -44,14 +44,20 @@ import eu.riscoss.client.Callback;
 import eu.riscoss.client.JsonModelList;
 import eu.riscoss.client.ModelInfo;
 import eu.riscoss.client.RiscossJsonClient;
+import eu.riscoss.client.SimpleRiskCconf;
 
 public class ModelSelectionDialog {
 		
 		DialogBox dialog;
 		Set<String> selection = new HashSet<String>();
+		SimpleRiskCconf rc;
+		String layer;
 		
 		Callback<List<String>> callback;
 		
+		//Used in WhatIfAnalysis, duplicated method, 
+		//right now both are needed, needs to be adapted and leave
+		//just one of them
 		public void show( Callback<List<String>> cb ) {
 			
 			this.callback = cb;
@@ -85,6 +91,75 @@ public class ModelSelectionDialog {
 								}
 							}
 						});
+						grid.setWidget( i, 0, chk );
+					}
+					DockPanel dock = new DockPanel();
+					dock.add( grid, DockPanel.CENTER );
+					Button ok = new Button( "Ok", new ClickHandler() {
+						@Override
+						public void onClick(ClickEvent event) {
+							dialog.hide();
+							if( callback != null ) {
+								callback.onDone( new ArrayList<String>( selection ) );
+							}
+						}
+					});
+					Button cancel = new Button ("Cancel", new ClickHandler() {
+						@Override
+						public void onClick(ClickEvent event) {
+							dialog.hide();
+						}
+					});
+					ok.setStyleName("deleteButton");
+					cancel.setStyleName("deleteButton");
+					HorizontalPanel buttons = new HorizontalPanel();
+					buttons.add(ok);
+					buttons.add(cancel);
+					dock.add( buttons , DockPanel.SOUTH );
+					dialog.add( dock );
+					dialog.getElement().getStyle().setZIndex( Integer.MAX_VALUE );
+					dialog.show();
+				}} );
+		}
+		
+		//Used in RiskConf management
+		public void show( String l, SimpleRiskCconf rconf,  Callback<List<String>> cb ) {
+			
+			this.callback = cb;
+			this.rc = rconf;
+			this.layer = l;
+			
+			RiscossJsonClient.listModels( new JsonCallback() {
+				@Override
+				public void onFailure(Method method, Throwable exception) {
+					Window.alert( exception.getMessage() );
+				}
+				@Override
+				public void onSuccess(Method method, JSONValue response) {
+					JsonModelList list = new JsonModelList( response );
+					List<String> l = rc.getModelList(layer);
+					dialog = new DialogBox( true, true ); //, new HtmlCaption( "Add model" ) );
+					dialog.setText( "Model Selection" );
+					Grid grid = new Grid();
+					grid.resize( list.getModelCount(), 1 );
+					for( int i = 0; i < list.getModelCount(); i++ ) {
+						ModelInfo info = list.getModelInfo( i );
+						CheckBox chk = new CheckBox( info.getName() );
+						chk.setName( info.getName() );
+						chk.addClickHandler( new ClickHandler() {
+							@Override
+							public void onClick(ClickEvent event) {
+								CheckBox chk = (CheckBox)event.getSource();
+								boolean value = chk.getValue();
+								if( value == true ) {
+									selection.add( chk.getName() );
+								}
+								else {
+									selection.remove( chk.getName() );
+								}
+							}
+						});
+						if (l.contains(info.getName())) chk.setChecked(true);
 						grid.setWidget( i, 0, chk );
 					}
 					DockPanel dock = new DockPanel();
