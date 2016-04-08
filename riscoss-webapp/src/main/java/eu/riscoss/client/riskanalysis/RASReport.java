cb58268d29e5cc7d0cd64f0fddc64357bfe686b4
quim-motger <quim.motger@gmail.com>
2016-04-08 18:51:06 +0200

    report generation fixed

--- riscoss-webapp/src/main/java/eu/riscoss/client/riskanalysis/RASReport.java
+++ riscoss-webapp/src/main/java/eu/riscoss/client/riskanalysis/RASReport.java
@@ -4,25 +4,27 @@ import org.fusesource.restygwt.client.JsonCallback;
 import org.fusesource.restygwt.client.Method;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.json.client.JSONValue;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 import eu.riscoss.client.Log;
 import eu.riscoss.client.RiscossJsonClient;
 
 public class RASReport implements EntryPoint {
 
-	SimplePanel simplePanel;
+	VerticalPanel vPanel;
 	
 	@Override
 	public void onModuleLoad() {
 		Log.println("Load module");
 		String selectedRAS = Window.Location.getParameter("id");
 		Log.println("Get parameter id");
-		simplePanel = new SimplePanel();
+		vPanel = new VerticalPanel();
+		vPanel.setWidth("100%");
 		
 		RiscossJsonClient.generateHTMLReport(selectedRAS, new JsonCallback() {
 			@Override
@@ -36,12 +38,12 @@ public class RASReport implements EntryPoint {
 		           
 		        HTMLPanel htmlPanel = new HTMLPanel(htmlString);
 		        
-		        simplePanel.setWidget(htmlPanel);
+		        vPanel.add(htmlPanel);
 		        Log.println("Integrated");
 			}
 		});
 		
-		RootPanel.get().add(simplePanel);
+		RootPanel.get().add(vPanel);
 	}
 
 }
